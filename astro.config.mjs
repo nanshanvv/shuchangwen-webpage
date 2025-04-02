61cf6ef1b61b240e7b39fb337b2de974e9dbf084
nanshanvv <shuchangwen0922@gmail.com>
2025-04-02 17:18:44 -0400

    deploy to gihub

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,8 +21,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: 'https://nanshanvv.github.io/shuchangwen-webpage',
+  base: '/shuchangwen-webpage/',
   trailingSlash: "always",
   integrations: [
     tailwind(
